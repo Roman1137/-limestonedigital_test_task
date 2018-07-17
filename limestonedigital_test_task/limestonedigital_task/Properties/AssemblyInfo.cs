2d307a018a3500e97e5f23778f86a4686d386089
Roman <poma.borodavka@gmail.com>
2018-07-17 16:19:37 +0300

    set up the repositry

---
+++ limestonedigital_test_task/limestonedigital_task/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("limestonedigital_task")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Win-Torrent Yagd")]
+[assembly: AssemblyProduct("limestonedigital_task")]
+[assembly: AssemblyCopyright("Copyright © Win-Torrent Yagd 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1248f79d-2c7d-48bf-bcae-c3547349af0f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ limestonedigital_test_task/limestonedigital_task/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace limestonedigital_task
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
